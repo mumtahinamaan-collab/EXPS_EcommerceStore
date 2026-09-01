26cdab55b1f726859cc0f08040be533d07eaa430
Mumtahina Naaeem <maanmumtahina@gmail.com>
2026-09-01 13:21:07 +0500

    Fix Gmail SMTP SSL settings

--- backend/settings.py
+++ backend/settings.py
@@ -18,15 +18,14 @@ FRONTEND_URL = os.getenv("FRONTEND_URL")
 # ==================================================
 # EMAIL SETTINGS
 # ==================================================
-# ==================================================
-# EMAIL SETTINGS
-# ==================================================
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "465"))
+
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
